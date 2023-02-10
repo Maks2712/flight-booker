a4264da30734a10f6cbf2b3ad54171aa75b747f3
Maks2712 <maks2712@yahoo.com>
2023-02-10 19:30:18 +0700

    adding search results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,18 +1,21 @@
 class FlightsController < ApplicationController
-  #before_action :flight_params
+  before_action :flight_params
   def index 
-    #@flights=Flight.all
+   
     @departure_airport = Airport.all.map{|a| [a.code, a.id]}
-    @arrival_airport = Airport.all.map{ |a| [ a.code, a.id ] }
-    @flight_date = Flight.all.map{ |a| [ a.start_datetime, a.id ] }
-   @flights = Flight.where("arrival_airport_id = ? AND departure_airport_id = ?", params[:flight][:arrival_airport_id],params[:flight][:departure_airport_id])
+    @arrival_airport = Airport.all.map{ |a| [a.code, a.id] }
+   @flights = Flight.where("departure_airport_id = ?", 
+  params[:flight][:departure_airport_id]).where("arrival_airport_id = ?", 
+  params[:flight][:arrival_airport_id])
   end
   
 private
-  def event_date_formatted
-    event_date.strftime("%m/%d/%Y") 
-  end
+
+  
   def flight_params
-    params.require(:flight).permit(:flight_duration, :departure_airport_id, :arrival_airport_id, :start_date)
+    #params.require(:flight).permit(:flight_duration, :departure_airport_id, :arrival_airport_id, :start_date)
+  params.permit(:flight_duration, :departure_airport_id, :arrival_airport_id, 
+  :start_date, :flight, :flights)
   end
+
 end
